17c64b40031c67fc6d47a6f297e8da78185fe151
Cesario Auditya Pratama Putra <cesarioauditya@gmail.com>
2020-12-15 09:21:26 +0700

    Create attitudepoint.php

---
+++ lang/en/attitudepoint.php
@@ -0,0 +1,78 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+
+/**
+ * English strings for attitudepoint
+ *
+ * You can have a rather longer description of the file as well,
+ * if you like, and it can span multiple lines.
+ *
+ * @package    mod_attitudepoint
+ * @copyright  "2020 Ydham Halid"
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['modulename'] = 'attitudepoint';
+$string['modulenameplural'] = 'attitudepoints';
+$string['modulename_help'] = 'attitudepoint help';
+$string['attitudepointfieldset'] = 'Custom example fieldset';
+$string['attitudepointname'] = 'attitudepoint name';
+$string['attitudepointname_help'] = 'This is the content of the help tooltip associated with the attitudepointname field. Markdown syntax is supported.';
+$string['attitudepoint'] = 'attitudepoint';
+$string['pluginadministration'] = 'attitudepoint Administration';
+$string['pluginname'] = 'attitudepoint Activity';
+$string['someadminsetting'] = 'Some Admin Setting';
+$string['someadminsetting_details'] = 'More info about Some Admin Setting';
+$string['someinstancesetting'] = 'Some Instance Setting';
+$string['someinstancesetting_details'] = 'More infor about Some Instance Setting';
+$string['attitudepointsettings'] = 'attitudepoint settings';
+$string['attitudepoint:addinstance'] = 'Add a new attitudepoint';
+$string['attitudepoint:view'] = 'View attitudepoint';
+$string['attitudepoint:preview'] = 'Preview attitudepoint';
+$string['attitudepoint:itemview'] = 'View items';
+$string['attitudepoint:itemedit'] = 'Edit items';
+$string['id']='ID';
+$string['name']='Name';
+$string['timecreated']='Time Created';
+$string['basicheading']='Basic Report';
+$string['overview']='Overview';
+$string['overview_help']='Overview Help';
+$string['view']='View';
+$string['preview']='Preview';
+$string['viewreports']='View Reports';
+$string['reports']='Reports';
+$string['basicreport']='Basic Report';
+$string['returntoreports']='Return to Reports';
+$string['exportexcel']='Export to CSV';
+$string['mingradedetails'] = 'The minimum grade required to "complete" this activity.';
+$string['mingrade'] = 'Minimum Grade';
+$string['deletealluserdata'] = 'Delete all user data';
+$string['maxattempts'] ='Max. Attempts';
+$string['unlimited'] ='unlimited';
+$string['gradeoptions'] ='Grade Options';
+$string['gradenone'] ='No grade';
+$string['gradelowest'] ='lowest scoring attempt';
+$string['gradehighest'] ='highest scoring attempt';
+$string['gradelatest'] ='score of latest attempt';
+$string['gradeaverage'] ='average score of all attempts';
+$string['defaultsettings'] ='Default Settings';
+$string['exceededattempts'] ='You have completed the maximum {$a} attempts.';
+$string['attitudepointtask'] ='attitudepoint Task';
+$string['missingidandcmid']='You must specify a course_module ID or an instance ID';
